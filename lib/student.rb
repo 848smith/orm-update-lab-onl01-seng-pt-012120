1ae0389b4395b07ff24135e2fd84d23a777be83d
Andrew Smith <848smith@gmail.com>
2020-04-15 02:37:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,10 @@ class Student
   
   def self.new_from_db(row)
     student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
   
   def self.find_by_name
