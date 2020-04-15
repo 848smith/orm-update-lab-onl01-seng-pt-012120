fba7af9da0d5f3aafaa245b1657891f7e458e5f4
Andrew Smith <848smith@gmail.com>
2020-04-15 02:40:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,12 +46,10 @@ class Student
     student
   end
   
-  def self.new_from_db(row)
-    student = Student.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+  def self.new_from_db(name, grade)
+    sql = <<-SQL
+      SELECT * FROM 
+      SQL
   end
   
   def self.find_by_name
