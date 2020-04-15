d3a0450ff8db9cb66e61e0bda610b6582b654e46
Andrew Smith <848smith@gmail.com>
2020-04-15 02:14:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,18 @@ class Student
     @id = id
   end
   
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+      SQL
+    DB[:conn].execute(sql)
+  end
   
-  
+  def self.drop_table
+    
+  end
 end
